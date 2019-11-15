c0eaab40502dc7d2d9dd36120104f11c33901cb0
Wanderley <wanderleybc128@gmail.com>
2019-11-14 20:15:20 -0500

    Crear camara FPS
    Movimiento

---
+++ Assets/FPSAim.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSAim : MonoBehaviour
+{
+    
+    float mouseX; // Variable para la posición en X
+    float mouseY; // Variable para la posición en Y
+    public bool InvertedMouse; // CheckBox usado para invertir la cámara
+    public GameObject player; // Se crea para ingresarle el movimiento a la cámara.
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Función ingresada para evitar la aparicón del cursor en pantalla.
+    }
+
+    
+    void Update()
+    /*
+    Bloque de código en el cuál se le dan las órdenes de movimiento en eje X y Y, además se indican las acciones a realizar cuando se activa el CheckBox. 
+    El Debug lo usamos para imprimir los movimientos de X.
+    */
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        if (InvertedMouse)
+        {
+            mouseY += Input.GetAxis("Mouse Y");
+        } else
+
+        {
+            mouseY -= Input.GetAxis("Mouse Y");
+        }
+
+        Debug.Log(mouseX);
+
+        transform.eulerAngles = new Vector3(mouseY,mouseX ,0);
+        player.transform.eulerAngles = new Vector3(0,mouseX ,0);
+    /*
+    Las opciones anteriores se ingresan para asignar valores en relación al movimiento y el objeto. 
+     */
+    }  
+         
+}
+    
---
+++ Assets/FPSmove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSmove : MonoBehaviour
+{   
+    public float speed = 0.1f; // Se ingresa la velocidad a la cuál se moverá el objeto. 
+    void Start()
+    {
+
+    }
+    void Update()
+
+    /*
+    
+    En este bloque de código se asignan los movimientos de la forma WASD al teclado. De la siguiente forma: W: adelante, A: izquierda, S: atrás, D: derecha. 
+
+     */
+    {
+        if (Input.GetKey (KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
+        }
+    }
+}
